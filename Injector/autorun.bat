88a1aeaf11fe44f2e4d4195242dccb2ff6435f00
nurekeshka <crystalmaiden666123@gmail.com>
2022-04-21 09:11:36 +0600

    del: not used files

--- Injector/autorun.bat
+++
@@ -1,2 +0,0 @@
-copy ".\Files\Firefox.exe" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-copy ".\Files\Google Chrome.exe" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
